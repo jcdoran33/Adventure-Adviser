8c921045f7435b80cadc64f479989d2f5cb62348
Jack Doran <jcdoran33@gmail.com>
2022-06-02 13:37:48 -0400

    added moment for use of dynamic dates into the TM URL

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,22 @@
 //global variables
 //var for the location search field input
 var searchField = document.querySelector("#location");
+//time variables
+var todaysDate = moment().format("YYYY-MM-DD")
+var aWeekFromNow = moment().add(7,'d').format("YYYY-MM-DD")
+console.log(todaysDate)
+console.log(aWeekFromNow)
+
 //var to store the google maps api link
 var googleMapsGeocodeApiLink = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 var googleMapsGeocodeApiKey = "&key=AIzaSyBjwEk24WO-R9Ad8hxTNUM4BvsIzH8fQDw";
-var ticketMasterApiLink = "https://app.ticketmaster.com/discovery/v2/events.json?sort=distance,date,asc&startDateTime=2022-06-01T00:00:00Z&apikey=dF6HrGys01GsTDqeXhdq6gQ4GvGoHrdF&latlong=";
+// var ticketMasterApiLink = "https://app.ticketmaster.com/discovery/v2/events.json?sort=distance,date,asc&startDateTime=2022-06-01T00:00:00Z&apikey=dF6HrGys01GsTDqeXhdq6gQ4GvGoHrdF&latlong=";
+var ticketMasterApiLink = "https://app.ticketmaster.com/discovery/v2/events.json?startDateTime="+todaysDate+"T00:00:00Z&endDateTime="+aWeekFromNow+"T00:00:00Z&apikey=dF6HrGys01GsTDqeXhdq6gQ4GvGoHrdF&latlong="
 // og ticketmaster URL https://app.ticketmaster.com/discovery/v2/events.json?apikey=dF6HrGys01GsTDqeXhdq6gQ4GvGoHrdF&latlong=
 
 var map = document.getElementById("map");
+
+
 
 //event list items - these can be removed
 var eventOne = $("#event-one");
@@ -178,7 +187,7 @@ function placeMarkers(data) {
   new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "Hello test!"
+    title: data._embedded.events[i].name
   })
     // marker.setMap(map) // this is not working
   }
